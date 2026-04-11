2472fc2a4cf13fc0d04d0610bac2869894108823
Bielikov Maksym <maximax6767@gmail.com>
2026-04-11 10:28:10 +0300

    fix: static assets not loading

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath: isProd ? '/stm32f103-exam-prep' : '',
+  assetPrefix: isProd ? '/stm32f103-exam-prep' : '',
 };
 
 export default nextConfig;
